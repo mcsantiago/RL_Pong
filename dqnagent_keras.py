d1d5f473b5c7789b1fc4080ffb6a43841d72cc68
Matthew Santiago <carag.matthew@gmail.com>
2020-05-01 20:21:05 -0500

    MobileNetV2 Experiment

--- dqnagent_keras.py
+++ dqnagent_keras.py
@@ -1,12 +1,16 @@
 import random
 import numpy as np
 from collections import deque
-from keras import backend as K
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatten
-from keras.optimizers import RMSprop
+
+# tensorflow stuff
+import tensorflow as tf
+from tensorflow import keras
+
+from hyperparameters import TRAINING_BN_MOMENTUM, TRAINING_BN_EPSILON
 
 # reward discount used by Karpathy (cf. https://gist.github.com/karpathy/a4166c7fe253700972fcbc77e4ea32c5)
+
+
 def discount_rewards(r, gamma):
     """ take 1D float array of rewards and compute discounted reward """
     r = np.array(r)
@@ -14,48 +18,116 @@ def discount_rewards(r, gamma):
     running_add = 0
     # we go from last reward to first one so we don't have to do exponentiations
     for t in reversed(range(0, r.size)):
-        if r[t] != 0: running_add = 0 # if the game ended (in Pong), reset the reward sum
-        running_add = running_add * gamma + r[t] # the point here is to use Horner's method to compute those rewards efficiently
+        if r[t] != 0:
+            # if the game ended (in Pong), reset the reward sum
+            running_add = 0
+        # the point here is to use Horner's method to compute those rewards efficiently
+        running_add = running_add * gamma + r[t]
         discounted_r[t] = running_add
-    discounted_r -= np.mean(discounted_r) #normalizing the result
-    discounted_r /= np.std(discounted_r) #idem
+    discounted_r -= np.mean(discounted_r)  # normalizing the result
+    discounted_r /= np.std(discounted_r)  # idem
     return discounted_r
+
 
 class DQNAgent:
     ''' Keras implementation of the paper '''
+
     def __init__(self, state_size, action_size):
-        print(K.tensorflow_backend._get_available_gpus())
+        device_name = tf.test.gpu_device_name()
+        if device_name != '/device:GPU:0':
+            raise SystemError('GPU device not found')
+        print('Found GPU at: {}'.format(device_name))
+
         self.state_size = state_size
         self.action_size = action_size
-        
-        self.memory = deque(maxlen=160000)
-        
+
+        print('action_size: ' + str(self.action_size))
+
+        self.memory = deque(maxlen=80000)
+
         self.gamma = 0.95
-        
+
         self.epsilon = 1.0
         self.epsilon_decay = 0.000009
         self.epsilon_min = 0.1
-        
+
         self.learning_rate = 0.0001
-        
+
         self.model = self._build_model()
-        
+
+    def bottleneck_block(self, k, t, c, n, s, x, padding='same'):
+        """ Bottleneck block with:
+            k input channels
+            t expension
+            c output channels
+            n levels
+            s strides on the first level
+            x input layer
+        """
+        for n0 in range(n):
+            residual = keras.layers.BatchNormalization(
+                axis=-1, momentum=TRAINING_BN_MOMENTUM, epsilon=TRAINING_BN_EPSILON, center=True, scale=True)(x)
+            residual = tf.nn.relu6(residual)
+            residual = keras.layers.Conv2D(
+                k, 1, strides=1, padding='same', activation=None, use_bias=False)(residual)
+
+            residual = keras.layers.BatchNormalization(
+                axis=-1, momentum=TRAINING_BN_MOMENTUM, epsilon=TRAINING_BN_EPSILON, center=True, scale=True)(residual)
+            residual = tf.nn.relu6(residual)
+            residual = keras.layers.DepthwiseConv2D(
+                3, strides=1, padding='same', activation=None, use_bias=False, depth_multiplier=t)(residual)
+
+            residual = keras.layers.BatchNormalization(
+                axis=-1, momentum=TRAINING_BN_MOMENTUM, epsilon=TRAINING_BN_EPSILON, center=True, scale=True)(residual)
+            residual = tf.nn.relu6(residual)
+            residual = keras.layers.Conv2D(
+                c, 1, strides=1, padding='same', activation=None, use_bias=False)(residual)
+
+            x = keras.layers.Conv2D(
+                c, 1, strides=1, padding='same', activation=None, use_bias=False)(x)
+            x = keras.layers.Add()([x, residual])
+        return x
+
     def _build_model(self):
-        model = Sequential()
-    
-        model.add(Conv2D(16, kernel_size=8, activation='relu', kernel_initializer='glorot_uniform', input_shape=(80, 80, 4)))
-        model.add(Conv2D(32, kernel_size=4, strides=2, kernel_initializer='glorot_uniform', activation='relu'))
-        model.add(Flatten())
-        model.add(Dense(256, activation='relu', kernel_initializer='glorot_uniform'))
-        model.add(Dense(self.action_size, activation='linear'))
-        
-        model.compile(loss='mse', optimizer=RMSprop(lr=self.learning_rate))
-        
+        ''' Builds a MobileNetv2 model '''
+        model_input = keras.Input(shape=(80, 80, 4), name='input_image')
+        x = model_input
+
+        x = keras.layers.Conv2D(32, 3, strides=2, padding='same',
+                                activation=None, use_bias=False)(x)
+        x = self.bottleneck_block(32, 1, 16, 1, 1, x)
+        x = self.bottleneck_block(16, 6, 24, 2, 2, x)
+        # x = self.bottleneck_block(24, 6, 32, 3, 2, x)
+        # x = self.bottleneck_block(32, 6, 64, 4, 2, x)
+        # x = self.bottleneck_block(64, 6, 96, 3, 1, x)
+        # x = self.bottleneck_block(96, 6, 160, 3, 2, x)
+        # x = self.bottleneck_block(96, 6, 320, 1, 1, x)
+
+        x = keras.layers.Conv2D(
+            256, 1, strides=1, padding='valid', activation='relu', use_bias=False)(x)
+
+        # encoder - output
+        encoder_output = x
+
+        # decoder
+        y = keras.layers.GlobalAveragePooling2D()(encoder_output)
+        decoder_output = keras.layers.Dense(
+            self.action_size, activation='softmax')(y)
+
+        # forward path
+        model = keras.Model(inputs=model_input,
+                            outputs=decoder_output, name='mobilenetv2')
+
+        # loss, backward path (implicit) and weight update
+        model.compile(optimizer=tf.keras.optimizers.Adam(
+            self.learning_rate), loss='binary_crossentropy', metrics=['accuracy'])
+
+        # return model
         return model
-    
+
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
-        
+
     def act(self, state):
         if self.epsilon > self.epsilon_min:
             self.epsilon -= self.epsilon_decay
@@ -64,26 +136,23 @@ class DQNAgent:
         act_values = self.model.predict(state)
         print('{}, action: {}'.format(act_values, np.argmax(act_values[0])))
         return np.argmax(act_values[0])
-    
+
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
-        # rewards = [x[2] for x in minibatch]
-        # d_rewards = discount_rewards(rewards, self.gamma)
-        # print(d_rewards)
-        
+
         for i, (state, action, reward, next_state, done) in enumerate(minibatch):
             target = reward
             if not done:
-                target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
-                # print('reward {} target {}'.format(reward, target))
-                # print(target)
+                target = reward + self.gamma * \
+                    np.amax(self.model.predict(next_state)[0])
+                print('reward {} target {} action {}'.format(
+                    reward, target, action))
             target_f = self.model.predict(state)
             target_f[0][action] = target
-            # print(target_f[0])
-            self.model.fit(state, target_f, epochs=1, verbose=0)
-    
+            self.model.fit(state, target_f, batch_size=32, epochs=1, verbose=0)
+
     def load(self, name):
         self.model.load_weights(name)
-        
+
     def save(self, name):
         self.model.save_weights(name)
--- main.py
+++ main.py
@@ -7,25 +7,28 @@ from matplotlib import pyplot
 
 from dqnagent_keras import DQNAgent
 
+
 def rgb2gray(rgb):
-    small_frame = np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+    small_frame = np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
     return small_frame
 
+
 def preprocess_frame(state):
-    state = state[35:195]
-    state = rgb2gray(state)
-    state = state[::2,::2]
-    state[state < 100] = 1
-    state[state >= 100] = 0
+    state = state[35:195]       # Crop
+    state = rgb2gray(state)     # Grayscale
+    state = state[::2, ::2]      # Downsample by 2
+    # state[state < 100] = 1      # To binary
+    # state[state >= 100] = 0
     # pyplot.matshow(state)
     # pyplot.show()
     state = np.expand_dims(state, axis=0)
     state = np.expand_dims(state, axis=3)
-    state = state.astype('b')
+    # state = state.astype('b')
     return state
 
+
 if __name__ == "__main__":
-    env = gym.make('Assault-v0')
+    env = gym.make('Breakout-v0')
     state_size = env.observation_space
     action_size = env.action_space.n
 
@@ -41,7 +44,7 @@ if __name__ == "__main__":
     time = 0
     episode = 0
     max_score = 0
-    k = 4 # The paper mentions only registering every kth frame
+    k = 4  # The paper mentions only registering every kth frame
     while time < n_frames:
         next_state = preprocess_frame(env.reset())
 
@@ -50,19 +53,24 @@ if __name__ == "__main__":
         enemy_score = 0
         action = 0
         done = False
-        
+
         while not done:
             env.render()
             next_state, state_reward, done, _ = env.step(action)
             next_state = preprocess_frame(next_state)
-            if state_reward > 0: player_score += state_reward
-            else: enemy_score -= state_reward
+            if state_reward > 0:
+                player_score += state_reward
+            else:
+                enemy_score -= state_reward
 
             while next_state.shape[3] < k and not done:
                 next_frame, reward, done, _ = env.step(action)
-                next_state = np.append(next_state, preprocess_frame(next_frame), axis=3)
-                if reward > 0: player_score += reward
-                else: enemy_score -= reward
+                next_state = np.append(
+                    next_state, preprocess_frame(next_frame), axis=3)
+                if reward > 0:
+                    player_score += reward
+                else:
+                    enemy_score -= reward
                 state_reward += reward
 
             if state is not None and next_state.shape[3] == k and state.shape[3] == k:
@@ -74,16 +82,18 @@ if __name__ == "__main__":
             time += 1
         episode += 1
 
-        if player_score > max_score: 
+        if player_score > max_score:
             max_score = player_score
-        
-        print("frame: {}/{}    enemy_score: {}    player_score: {}    max_score: {}    e: {:.2}" # print the episode's score and agent's epsilon
-        .format(time, n_frames, enemy_score, player_score, max_score, agent.epsilon))
-        
+
+        print("frame: {}/{}    enemy_score: {}    player_score: {}    max_score: {}    e: {:.2}"  # print the episode's score and agent's epsilon
+              .format(time, n_frames, enemy_score, player_score, max_score, agent.epsilon))
+
         done = False
-        
+
         if len(agent.memory) > batch_size:
-            agent.replay(batch_size) # train the agent by replaying the experiences of the episode
-        
-        if episode % 50 == 0: # save weights every 50th episode (game)
-            agent.save(output_dir + "weights_" + '{:04d}'.format(episode) + ".hdf5")
+            # train the agent by replaying the experiences of the episode
+            agent.replay(batch_size)
+
+        if episode % 50 == 0:  # save weights every 50th episode (game)
+            agent.save(output_dir + "weights_" +
+                       '{:04d}'.format(episode) + ".hdf5")
